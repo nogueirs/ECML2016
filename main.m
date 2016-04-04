5a2ed0a302b3a0a3ea09b068f4e67b29f08c24eb
Sarah Nogueira <srh.nogueira@gmail.com>
2016-04-04 20:53:12 +0200

    Update main.m

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ function [] = main(rho)
 %%% ones as in the paper
 d=100; % total number of features
 d_rel=50; % number of relevant features
-num_instances=1000; % number of instances desired 
+num_instances=2000; % number of instances desired 
 num_lambdas=100;
 
 M=100;  % number of bootstrap samples to use to estimate stability
@@ -16,7 +16,7 @@ M=100;  % number of bootstrap samples to use to estimate stability
 labels(labels==-1)=0; %% make the labels 0 and 1 so that it can be used with the lassoglm function...
 
 %%% We run the experiments presented in the paper
-[error,av_error,logloss,av_logloss,num_lambdas,lambdas,stabilities,hat_pf,A]=experiment(data,labels,M,num_lambdas);
+[error,av_error,logloss,av_logloss,num_lambdas,lambdas,stabilities,hat_pf,A,featureSet]=experiment(data,labels,M,num_lambdas);
 %%% to plot the figures in the paper
 plotResults(av_error,av_logloss,lambdas,stabilities,hat_pf);
 
