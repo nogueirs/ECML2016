4c9abaed3a1c27679a1aef2b4e104742a64209ca
Sarah Nogueira <srh.nogueira@gmail.com>
2016-04-04 21:01:06 +0200

    Update main.m

--- main.m
+++ main.m
@@ -8,7 +8,7 @@ d=100; % total number of features
 d_rel=50; % number of relevant features
 num_instances=2000; % number of instances desired 
 num_lambdas=100;
-
+trainSize=floor(num_instances/2);
 M=100;  % number of bootstrap samples to use to estimate stability
 
 %% We generate the dataset using the parameters given above
@@ -16,9 +16,15 @@ M=100;  % number of bootstrap samples to use to estimate stability
 labels(labels==-1)=0; %% make the labels 0 and 1 so that it can be used with the lassoglm function...
 
 %%% We run the experiments presented in the paper
-[error,av_error,logloss,av_logloss,num_lambdas,lambdas,stabilities,hat_pf,A,featureSet]=experiment(data,labels,M,num_lambdas);
+[error,av_error,logloss,av_logloss,num_lambdas,lambdas,stabilities,hat_pf,A,featureSet]=experiment(data(1:trainSize,:),labels(1:trainSize),M,num_lambdas);
 %%% to plot the figures in the paper
 plotResults(av_error,av_logloss,lambdas,stabilities,hat_pf);
+
+%%% To get the final feature set on the remaining half of the data for a specific lambda
+%%% Set the lambda wanted and uncomment the lines hereafter
+%lambda=?
+%[B,FitInfo]=lassoglm(data(trainSize+1:end,:),labels(trainSize+1:end),'binomial','Lambda',lambda);
+%featureSet=B~=0;
 
 end
 
